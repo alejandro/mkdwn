7f808052e0964c3cf82d0335e2eea8247721e1e8
Alejandro Morales <vamg008@gmail.com>
2012-12-05 22:44:05 -0600

    [dist] Vim keybindigs implemented (close #1)
    - Keybindings are implemented

--- mkdwn/js/app.js
+++ mkdwn/js/app.js
@@ -1,27 +1,31 @@
+/*global Rainbow, define, ace, $, document, localStorage, window, marked, ui*/
+/* jshint browser:true*/ 'use strict';
 var App =  {}
-$(document).ready(function (){  
-  App.utils = App.u =  {
-    id : function id(l){
+$(document).ready(function (){
+  var _ = App.utils = App.u =  {
+    id: function id(l) { // Author @rem, from jsbin
       var vowels = 'aeiouAEIOU',
         consonants = 'bcdfghjklmnpqrstvwxyBCDFzLMNSXPRTW',
-        word = '', length = l || 6, index = 0, set;
+        word = '', length = l || 6, index = 0, set
 
       for (; index < length; index += 1) {
-        set = (index % 2 === 0) ? vowels : consonants;
-        word += set[Math.floor(Math.random() * set.length)]; 
+        set = (index % 2 === 0) ? vowels : consonants
+        word += set[Math.floor(Math.random() * set.length)];
       }
 
-      return word;
+      return word
     },
-    nextTick: function (fn){
-      setTimeout(fn, 0)
+
+    nextTick: function (fn){ // this is silly
+      return setTimeout(fn, 0)
     },
+
     defineProperty: function (el, property, val, set){
       if (typeof(property) === 'object') {
         var keys = Object.keys(property)
         return keys.forEach(function (k){
           console.log(k, property[k])
-          return defineProperty(el, k, keys[k])
+          return _.defineProperty(el, k, keys[k])
         })
       }
       Object.defineProperty(el, property, {
@@ -31,7 +35,6 @@ $(document).ready(function (){
       })
     }
   }
-  
 
   App.initialize = function (){
     marked.setOptions({
@@ -43,33 +46,33 @@ $(document).ready(function (){
    * Markdown Editor
    * -----------------
   */
-  App.Editor = function (){
+  App.Editor = function (editor){
     ui.Emitter.call(this)
-    this.initialize()
+    this.initialize(editor)
   }
   
-  App.Editor.prototype = App.Editor.fn = new ui.Emitter
+  App.Editor.prototype = App.Editor.fn = new ui.Emitter()
   
   App.Editor.include = function (o){
     Object.keys(o).forEach(function (i){
       return App.Editor.prototype[i] = o[i]
-    }.bind(this))
+    })
   }
   
 
   App.Editor.include({
-    initialize: function (){
+    initialize: function (editor){
       var emitChange = this.emit.bind(this, 'change')
-      this.id = (window.location.hash || App.utils.id()).replace('#','')
-      this.el = $('[name="textarea"]');
+      this.id = (window.location.hash || _.id()).replace('#','')
+      this.el = editor;
       
-      App.u.defineProperty(this, 'textContent', function (){
-        return this.el.val()
+      _.defineProperty(this, 'textContent', function (){
+        return this.el.getValue()
       }, function (t){
-        return this.el.val(t)
+        return this.el.getSession().setValue(t)
       })
 
-      this.setupStorage();
+      this.setupStorage()
       this.el.on('keypress', emitChange)
       this.el.on('change', emitChange)
       this.on('change', this.save.bind(this))
@@ -79,19 +82,22 @@ $(document).ready(function (){
     cleanCanvas : function (){
       this.textContent = ''
     },
+
     save: function (e){
       // let the event happen then fire this
-      App.u.nextTick(function(){ 
+      _.nextTick(function(){ 
         localStorage.setItem(this.id, JSON.stringify({
           id: this.id,
           textContent: this.textContent,
-          mtime: +new Date
+          mtime: +new Date()
         }))
       }.bind(this))
     },
+
     get: function (){
       return localStorage.getItem(this.id)
     },
+
     setupStorage: function (){
       var cval = this.get()
       if (!cval) {
@@ -100,7 +106,7 @@ $(document).ready(function (){
       } else {
         this.merge(JSON.parse(cval))
       }
-      App.u.nextTick(this.emit.bind(this,'change'))
+      _.nextTick(this.emit.bind(this,'change'))
     },
 
     merge: function (o){
@@ -135,20 +141,18 @@ $(document).ready(function (){
   App.Reader.prototype.parse = function() {
     var md = App.editor.textContent
     this.kue.push(1)
-    var leap = +new Date - this.last
+    var leap = +new Date() - this.last
 
-      App.u.nextTick(function (){
+      _.nextTick(function (){
         try { this.el.html(marked(md))} catch(ex){}
-        this.last = +new Date
+        this.last = +new Date()
         this.el.find('code').each(function (i, el){
-            var $el = $(el);
-
-            // Convert any smart quotes into dumb quotes.
-            var html = $el.html();
-            var leftSingleQuote = /‘/g;
-            var leftDoubleQuote = /“/g;
-            var singleQuote     = /’/g;
-            var doubleQuote     = /”/g;
+            var $el = $(el)
+              , html = $el.html()
+              , leftSingleQuote = /‘/g
+              , leftDoubleQuote = /“/g
+              , singleQuote     = /’/g
+              , doubleQuote     = /”/g
 
             html = html
                 .replace(leftSingleQuote, '\'')
@@ -165,7 +169,7 @@ $(document).ready(function (){
                     $el.attr('data-language', language);
               }
             })
-            try { Rainbow.color(); }catch(ex){}
+            try { Rainbow.color() }catch(ex){}
         })
         
       }.bind(this))
@@ -178,9 +182,9 @@ $(document).ready(function (){
     menu: ui.menu(),
     createDialog: function (title, body){
       return ui.dialog(title, body)
-                 .closable()
-                 .overlay()
-                 .show()
+               .closable()
+               .overlay()
+               .show()
     },
     rename: function (){
       var html = $('<input name="name" type="text" />')
@@ -195,14 +199,25 @@ $(document).ready(function (){
   /**
    * Setup
    */
-  App.editor = App.e = new App.Editor
-  App.reader = App.r = new App.Reader
+  define('editor', function(require) {
+    var editor = ace.edit("editor");
+    ace.config.set("workerPath", "components/ace/build/src");
+    editor.getSession().setMode("ace/mode/markdown");
+    editor.renderer.setShowGutter(false); 
+    editor.setKeyboardHandler(require('ace/keyboard/vim').handler)
+    return editor
+  })
+
+  window.Editor = require('editor')
+  App.editor = App.e = new App.Editor(window.Editor)
+  App.reader = App.r = new App.Reader()
   App.UI.menu
     .add('New &lt;alt+n&gt;', App.e.newCanvas)
     .add('Load &lt;alt+o&gt;', App.e.loadCanvas)
     .add('Set Name', App.e.setName)
     .add('Rename', App.UI.rename)
     .add('Clean all', App.e.cleanCanvas)
+  
   
   window.oncontextmenu = function(e){
     e.preventDefault();
